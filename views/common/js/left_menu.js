bcfb56b985cd93363fd937a9e5fa4bc9e6fc5e92
Tran Manh Quy <quytm2239@gmail.com>
2017-08-05 01:02:20 +0700

    Basic complete left menu

--- views/common/js/left_menu.js
+++ views/common/js/left_menu.js
@@ -1,6 +1,16 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $("button").on("click", function() {
-        console.log("asdadasd");
-    })
+
+    $("#left_menu_li_main").on("click", function() {
+        window.location = "/";
+    });
+    $("#left_menu_li_list").on("click", function() {
+        window.location = "/list";
+    });
+    $("#left_menu_li_dashboard").on("click", function() {
+        window.location = "/dashboard";
+    });
+    // $("#left_menu_li_4").on("click", function() {
+    //     window.location = "/list";
+    // });
 });
