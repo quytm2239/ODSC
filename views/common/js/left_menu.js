09dc2e9eff019cec7b9d051fd9fc97e480e4154e
Tran Manh Quy <quytm2239@gmail.com>
2017-08-06 18:56:51 +0700

    Adjust color theme

--- views/common/js/left_menu.js
+++ views/common/js/left_menu.js
@@ -11,4 +11,22 @@ $( document ).ready(function() {
     $("#left_menu_li_dashboard").on("click", function() {
         window.location = "/dashboard";
     });
+    var originalDisplay;
+    $(".left_menu_li").mouseenter(function() {
+        originalDisplay = $(this).find(".circle_inside").css("display");
+        $(this).find(".circle").css("background", "#545454");
+        $(this).find(".circle_inside").css("background", "#FFFFFF");
+        if($(this).find(".circle_inside").css("display") == 'none') {
+            $(this).find(".circle_inside").css("display", "block");
+        }
+        $(this).css("background", "#FFFFFF");
+        $(this).css("color", "#545454");
+    });
+    $(".left_menu_li").mouseleave(function() {
+        $(this).find(".circle").css("background", "#FFFFFF");
+        $(this).find(".circle_inside").css("background", "#545454");
+        $(this).find(".circle_inside").css("display", originalDisplay);
+        $(this).css("background", "#545454");
+        $(this).css("color", "#FFFFFF");
+    });
 });
